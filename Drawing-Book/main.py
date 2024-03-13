ddfccb38298865dd1e74c32af9fad4abc8716af2
ysraz singh <ysgahir1925@hotmail.com>
2024-03-13 21:19:56 +0530

    Drawing Book Solved

---
+++ Drawing-Book/main.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+
+def pageCount(n, p):
+    front = p//2
+    n += not (n % 2)
+    back = (n-p)//2
+    return front if front <= back else back
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
