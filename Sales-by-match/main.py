27f40ba3ac4117ccd7ae0759e8d785aff48cd8ca
ysraz singh <ysgahir1925@hotmail.com>
2024-03-13 20:33:33 +0530

    Sales by match: solbed

---
+++ Sales-by-match/main.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+
+def sockMerchant(n, ar):
+    pair_count = 0
+    pairs = set()
+
+    for color in ar:
+        if color in pairs:
+            pair_count += 1
+            pairs.remove(color)
+        else:
+            pairs.add(color)
+
+    return pair_count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
